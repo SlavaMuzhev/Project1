df03650d47c1e6577a51f3c7319a8b9a8e2d625c
Slava Muzhev <mzvslv@gmail.com>
2026-01-04 02:50:37 +0500

    add sort_by_date to processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,9 +1,16 @@
-from typing import List, Dict, Any
+from typing import Any, Dict, List
 
 
 def filter_by_state(list_of_dicts: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
-    """ Функция принимает список словарей и опционально значение для ключа state.
+    """Функция принимает список словарей и опционально значение для ключа state.
     Возвращает новый список словарей, содержащий только те
-    словари, у которых ключ state соответствует указанному значению """
-    filtered_list = [d for d in list_of_dicts if d.get('state') == state]
+    словари, у которых ключ state соответствует указанному значению"""
+    filtered_list = [d for d in list_of_dicts if d.get("state") == state]
     return filtered_list
+
+
+def sort_by_date(list_of_dicts: List[Dict[str, Any]], descending: bool = True) -> List[Dict[str, Any]]:
+    """Функция принимает список словарей и необязательный параметр, задающий порядок сортировки
+    (по умолчанию — убывание). Возвращает новый список отсортированный по дате (date)"""
+    sorted_list = sorted(list_of_dicts, key=lambda item: item["date"], reverse=descending)
+    return sorted_list
