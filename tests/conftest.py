552d963c0c7fd9e2193a781c9f31a3f0f755c34d
Slava Muzhev <mzvslv@gmail.com>
2026-01-25 19:51:42 +0500

    update type annotation

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,41 +1,39 @@
 import pytest
 
+
 @pytest.fixture
-def card_number():
+def card_number() -> str:
     return "1234123412341234"
 
 
 @pytest.fixture
-def account_number():
+def account_number() -> str:
     return "12341234123412341234"
 
 
 @pytest.fixture
-def account_details():
+def account_details() -> str:
     return "Счет 73654108430135874305"
 
 
 @pytest.fixture
-def data():
+def data() -> str:
     return "2024-03-11T02:26:18.671407"
 
 
 @pytest.fixture
-def same_date():
+def same_date() -> list:
     return [
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2019-07-03T02:08:58.425572'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2019-07-03T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2019-07-03T08:21:33.419441'}
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2019-07-03T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2019-07-03T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2019-07-03T08:21:33.419441"},
     ]
 
 
 @pytest.fixture
-def status_no_state():
+def status_no_state() -> list:
     return [
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
     ]
-
-
-
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,19 +1,19 @@
+from typing import Union
+
 import pytest
 
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 
-
-def test_get_mask_card_number(card_number):
+def test_get_mask_card_number(card_number: Union[str]) -> None:
     assert get_mask_card_number(card_number) == "1234 12** **** 1234"
 
 
 @pytest.mark.parametrize("data, expected", [("12341234123412341234", "**1234"), ("774521", "**4521")])
-def test_get_mask_account(data, expected):
+def test_get_mask_account(data: str, expected: str) -> None:
     assert get_mask_account(data) == expected
 
 
-def test_get_mask_card_number_fail():
+def test_get_mask_card_number_fail() -> None:
     with pytest.raises(ValueError, match="Номер карты должен содержать 16 цифр"):
         get_mask_card_number("456789")
-
