be0531cc6372df70dd158f4309db37df5a86ea9a
Slava Muzhev <mzvslv@gmail.com>
2026-01-25 18:19:27 +0500

    update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,6 +16,11 @@ def account_details():
 
 
 @pytest.fixture
+def data():
+    return "2024-03-11T02:26:18.671407"
+
+
+@pytest.fixture
 def same_date():
     return [
         {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
@@ -31,5 +36,6 @@ def status_no_state():
         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
     ]
+
 
 
