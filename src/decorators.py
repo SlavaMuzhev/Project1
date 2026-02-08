ae98bd60aaea91b9ea365f1d5e707110460f8c49
Slava Muzhev <mzvslv@gmail.com>
2026-02-08 16:31:19 +0500

    add tests/test_decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -1,25 +1,27 @@
 import functools
 import time
+from typing import Any, Callable, Optional
 
 
-def log(filename=None):
+def log(filename: Optional[str] = None) -> Callable[[Callable[..., Any]], Callable[..., Any]]:
     """
     Декоратор для логирования времени работы, результата или ошибок функции.
     """
-    def wrapper(func):
+
+    def wrapper(func: Callable[..., Any]) -> Callable[..., Any]:
         @functools.wraps(func)
-        def inner(*args, **kwargs):
+        def inner(*args: Any, **kwargs: Any) -> Any:
+            time_1 = time.time()
+            formatted_time_1 = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time_1))
             try:
-                time_1 = time.time()
-                formatted_time_1 = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time_1))
                 result = func(*args, **kwargs)
                 time_2 = time.time()
-                formatted_time_2 = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time_2))
-                log_message = f"""
-                {func.__name__} ok Результат: {result} 
-                Время начала: {formatted_time_1} Время завершения: {formatted_time_2}
-                Время работы: {time_2-time_1}
-                """
+                formatted_time_2 = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time_2))
+                log_message = (
+                    f"{func.__name__} ok Результат: {result}\n"
+                    f"Время начала: {formatted_time_1} Время завершения: {formatted_time_2}\n"
+                    f"Время работы: {time_2 - time_1:.7f}\n"
+                )
                 if filename:
                     with open(filename, "a", encoding="utf-8") as file:
                         file.write(log_message)
@@ -27,16 +29,19 @@ def log(filename=None):
                     print(log_message)
                 return result
             except Exception as e:
-                log_message = f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}"
+                time_err = time.time()
+                formatted_time_err = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time_err))
+                log_message = (
+                    f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}\n"
+                    f"Время начала: {formatted_time_1} Время завершения: {formatted_time_err}\n"
+                    f"Время работы: {time_err - time_1:.7f}\n"
+                )
                 if filename:
                     with open(filename, "a", encoding="utf-8") as file:
                         file.write(log_message)
+                else:
+                    print(log_message)
+
         return inner
+
     return wrapper
-
-
-@log(filename="mylog.txt")
-def my_function(x, y):
-    return x + y
-
-my_function(1, 2)
