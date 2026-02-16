1ddfaa36abd09cb6a1710a9c6cd68d5074e000ff
Slava Muzhev <mzvslv@gmail.com>
2026-02-16 18:08:05 +0500

    add src/utils.py

---
+++ src/utils.py
@@ -0,0 +1,17 @@
+import json
+import os
+
+
+def get_transactions_data(path):
+    """функцию, которая принимает на вход путь до JSON-файла и возвращает список словарей
+    с данными о финансовых транзакциях"""
+    if not os.path.exists(path) or os.path.getsize(path) == 0:
+        return []
+
+    try:
+        with open(path, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+
+        return data if isinstance(data, list) else []
+    except (json.JSONDecodeError, OSError):
+        return []
