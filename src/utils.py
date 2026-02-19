3f0cdb635b6b12aad1e2f5ee84bb4dbb7dfe3c72
Slava Muzhev <mzvslv@gmail.com>
2026-02-19 10:51:08 +0500

    update htmlcov

--- src/utils.py
+++ src/utils.py
@@ -1,20 +1,20 @@
 import json
 import os
+from typing import Any, List
 
 
-def get_transactions_data(path):
+def get_transactions_data(path: Any) -> List[Any]:
     """
-    функцию, которая принимает на вход путь до JSON-файла и возвращает список словарей
+    Функция, которая принимает на вход путь до JSON-файла и возвращает список словарей
     с данными о финансовых транзакциях
     """
     if not os.path.exists(path) or os.path.getsize(path) == 0:
         return []
 
     try:
-        with open(path, 'r', encoding='utf-8') as f:
+        with open(path, "r", encoding="utf-8") as f:
             data = json.load(f)
 
         return data if isinstance(data, list) else []
     except (json.JSONDecodeError, OSError):
         return []
-    
